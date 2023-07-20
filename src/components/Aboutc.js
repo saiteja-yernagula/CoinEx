3395f722c9b2f11ad02d0d2c78c44379b978678f
saiteja-yernagula <saiteja.yernagula@gmail.com>
2023-07-20 15:49:35 +0530

    firstst

--- src/components/Aboutc.js
+++ src/components/Aboutc.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Aboutc = () => {
   return (
-    <div>
+    <div className='overflow-hidden'>
       <div className='p-12 '>
       <div className='bg-blue-600 p-0 h-[70vh] rounded-lg rounded-[39px] text-white'>
       <div className='font-bold text-2xl sm:text-6xl pt-7  py-5 text-center sm:px-[200px] '>Are you interested in doing your project with us?</div>
-      <div className='text-center text-2xl m-6 text-black outline-none'><input type='text' placeholder='Enter Your Email' className='outline-none p-4 m-6'/><span className='bg-black text-white p-4'>contact now</span></div>
+      <div className='text-center text-1xl mt-4 m-2 text-black outline-none flex-wrap'><input type='text' placeholder='Enter Your Email' className='outline-none p-2 m-2'/><span className='bg-black text-white p-2'>contact now</span></div>
       
 
      
